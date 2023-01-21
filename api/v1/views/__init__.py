523abac36660c71febec5b96a16b1f352b80b7d5
MissTipo <dorine.a.tipo@gmail.com>
2023-01-21 13:19:05 +0300

    fix pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,4 +2,6 @@
 """Endpoint (route) will be to return the status of api"""
 from api.v1.views.index import *
 from flask import Blueprint
+
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
