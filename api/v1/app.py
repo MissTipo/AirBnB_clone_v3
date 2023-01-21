39d612475abec5d455abffbb27c8b7b8c6474979
MissTipo <dorine.a.tipo@gmail.com>
2023-01-21 13:59:38 +0300

    404 not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,12 +10,23 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """closes the current sqlalchemy session"""
     storage.close()
 
-if __name__=="__main__":
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Create a handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    """
+    return jsonify(error="Not found"), 404
+
+
+if __name__ == "__main__":
     host = getenv('HBNB_API_HOST') or '0.0.0.0'
     port = getenv('HBNB_API_PORT') or '5000'
     app.run(host=host, port=port, threaded=True)
