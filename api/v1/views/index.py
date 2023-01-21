2f3f6ac45760a3cb2bf1511d134c8ce2209f64d3
MissTipo <dorine.a.tipo@gmail.com>
2023-01-21 13:06:04 +0300

    add documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Endpoint (route) will be to return the status of API"""
 from api.v1.views import app_views
 from flask import *
 
 @app_views.route('/status', strict_slashes=False)
 def status():
+    """Returns a JSON of a string"""
     return jsonify({"status":"OK"})
